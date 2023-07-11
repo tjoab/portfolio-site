e88ffb2fdaf9f33a802e880fa4b9c25421b7446e
tjoab <tj.ayoub@gmail.com>
2023-07-11 14:26:00 -0400

    optimized about and work pages for mobile

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/Skills.css";
 import SkillElement from "./SkillElement";
 import TF from "../resources/TF.png";
@@ -11,42 +11,73 @@ import NN from "../resources/nn.png";
 import S from "../resources/stats.png";
 
 function Skills() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 560);
+    };
+
+    handleResize(); // Initial check
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="skillsWrap">
       <div className="skillsGrid">
         <div style={{ display: "contents" }}>
           <SkillElement
             imageSrc={Py}
-            area="1 / 5 / 1 / span 7"
+            area={isSmallScreen ? "1 / 3 / 1 / span 16" : "1 / 5 / 1 / span 7"}
             skill="Python"
           />
-          <SkillElement imageSrc={R} area="1 / 13 / 1 / span 7" skill="R" />
+          <SkillElement
+            imageSrc={R}
+            area={
+              isSmallScreen ? "1 / 21 / 1 / span 16" : "1 / 13 / 1 / span 7"
+            }
+            skill="R"
+          />
           <SkillElement
             imageSrc={TF}
-            area="1 / 21 / 1 / span 7"
+            area={isSmallScreen ? "2 / 3 / 2 / span 16" : "1 / 21 / 1 / span 7"}
             skill="Tensorflow"
           />
           <SkillElement
             imageSrc={PT}
-            area="1 / 29 / 1 / span 7"
+            area={
+              isSmallScreen ? "2 / 21 / 2 / span 16" : "1 / 29 / 1 / span 7"
+            }
             skill="PyTorch"
           />
           <SkillElement
             imageSrc={S}
-            area="2 / 5 / 2 / span 7"
+            area={isSmallScreen ? "3 / 3 / 3 / span 16" : "2 / 5 / 2 / span 7"}
             skill="Statistics"
           />
           <SkillElement
             imageSrc={brain}
-            area="2 / 13 / 2 / span 7"
+            area={
+              isSmallScreen ? "3 / 21 / 3 / span 16" : "2 / 13 / 2 / span 7"
+            }
             skill="Deep Learning"
           />
           <SkillElement
             imageSrc={NN}
-            area="2 / 21 / 2 / span 7"
+            area={isSmallScreen ? "4 / 3 / 4 / span 16" : "2 / 21 / 2 / span 7"}
             skill="Model Building"
           />
-          <SkillElement imageSrc={git} area="2 / 29 / 2 / span 7" skill="Git" />
+          <SkillElement
+            imageSrc={git}
+            area={
+              isSmallScreen ? "4 / 21 / 4 / span 16" : "2 / 29 / 2 / span 7"
+            }
+            skill="Git"
+          />
         </div>
       </div>
     </div>
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TS from "./styles/Timeline.module.css";
 import TimelineCard from "./TimelineCard";
 
 function Timeline() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 560);
+    };
+
+    handleResize(); // Initial check
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={TS.timelineHolder}>
       <div className={TS.timelineGrid}>
@@ -11,21 +26,21 @@ function Timeline() {
           title="Graduate Teaching Assistant"
           time="'22~"
           row="1 / 1"
-          columnSpan="16/span 9"
+          columnSpan={isSmallScreen ? "10/span 15" : "16/span 9"}
         />
         <TimelineCard
           company="Western University"
           title="Computer Algebra Researcher"
           time="'21"
           row="2 / 2"
-          columnSpan="8/span 7"
+          columnSpan={isSmallScreen ? "6/span 13" : "8/span 7"}
         />
         <TimelineCard
           company="Maplesoft"
           title="Software Developer"
           time="'20"
           row="3 / 3"
-          columnSpan="2/span 7"
+          columnSpan={isSmallScreen ? "2/span 10" : "2/span 7"}
         />
         <TimelineCard
           company="Freelance"

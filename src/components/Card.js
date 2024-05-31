c3329650c9793783164f31557d6c8893d6bc1486
TJ Ayoub <tjayoub@TJs-MacBook-Pro.local>
2024-05-30 23:31:33 -0400

    complete overhaul of responsive design, fixed design bugs

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,61 +3,39 @@ import "./styles/Card.css";
 import { Link } from "react-router-dom";
 
 function Card(props) {
-  const { projName, category, imageSrc, bgCol, start, end, workLink } = props;
-  const cardBoxRef = useRef(null);
+	const { projName, category, imageSrc, bgCol, start, end, workLink } = props;
+	const cardBoxRef = useRef(null);
 
-  const styling = {
-    background: bgCol,
-    gridColumnStart: start,
-    gridColumnEnd: end,
-  };
+	const styling = {
+		background: bgCol,
+		gridColumnStart: start,
+		gridColumnEnd: end,
+	};
 
-  useEffect(() => {
-    const updateCardImageSize = () => {
-      if (cardBoxRef.current) {
-        const cardBoxWidth = cardBoxRef.current.offsetWidth;
-        const cardBoxHeight = cardBoxRef.current.offsetHeight;
-
-        const cardImage = document.querySelectorAll(".card-image");
-
-        cardImage.forEach((cardImage) => {
-          cardImage.style.maxWidth = `${cardBoxWidth}px`;
-        });
-      }
-    };
-
-    window.addEventListener("resize", updateCardImageSize);
-    updateCardImageSize();
-
-    return () => {
-      window.removeEventListener("resize", updateCardImageSize);
-    };
-  }, []);
-
-  return (
-    <div className="card-box" style={styling} ref={cardBoxRef}>
-      <Link to={workLink} className="card">
-        <div className="card-wrap">
-          <div className="info">
-            <span className="proj-title">{category}</span>
-            <h2>{projName}</h2>
-          </div>
-          <div className="inner">
-            <div className="image-holder">
-              <figure>
-                <img
-                  className="card-image"
-                  src={imageSrc}
-                  alt="imgHolder"
-                  loading="lazy"
-                ></img>
-              </figure>
-            </div>
-          </div>
-        </div>
-      </Link>
-    </div>
-  );
+	return (
+		<div className="card-box" style={styling} ref={cardBoxRef}>
+			<Link to={workLink} className="card">
+				<div className="card-wrap">
+					<div className="info">
+						<span className="proj-title">{category}</span>
+						<h2>{projName}</h2>
+					</div>
+					<div className="inner">
+						<div className="image-holder">
+							<figure>
+								<img
+									className="card-image"
+									src={imageSrc}
+									alt="imgHolder"
+									loading="lazy"
+								></img>
+							</figure>
+						</div>
+					</div>
+				</div>
+			</Link>
+		</div>
+	);
 }
 
 export default Card;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -11,77 +11,77 @@ import NN from "../resources/nn.png";
 import S from "../resources/stats.png";
 
 function Skills() {
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
+	const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth < 560);
-    };
+	useEffect(() => {
+		const handleResize = () => {
+			setIsSmallScreen(window.innerWidth < 750);
+		};
 
-    handleResize();
+		handleResize();
 
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
-  return (
-    <div className="skillsWrap">
-      <div className="skillsGrid">
-        <div style={{ display: "contents" }}>
-          <SkillElement
-            imageSrc={Py}
-            area={isSmallScreen ? "1 / 3 / 1 / span 16" : "1 / 5 / 1 / span 7"}
-            skill="Python"
-          />
-          <SkillElement
-            imageSrc={R}
-            area={
-              isSmallScreen ? "1 / 21 / 1 / span 16" : "1 / 13 / 1 / span 7"
-            }
-            skill="R"
-          />
-          <SkillElement
-            imageSrc={TF}
-            area={isSmallScreen ? "2 / 3 / 2 / span 16" : "1 / 21 / 1 / span 7"}
-            skill="Tensorflow"
-          />
-          <SkillElement
-            imageSrc={PT}
-            area={
-              isSmallScreen ? "2 / 21 / 2 / span 16" : "1 / 29 / 1 / span 7"
-            }
-            skill="PyTorch"
-          />
-          <SkillElement
-            imageSrc={S}
-            area={isSmallScreen ? "3 / 3 / 3 / span 16" : "2 / 5 / 2 / span 7"}
-            skill="Statistics"
-          />
-          <SkillElement
-            imageSrc={brain}
-            area={
-              isSmallScreen ? "3 / 21 / 3 / span 16" : "2 / 13 / 2 / span 7"
-            }
-            skill="Deep Learning"
-          />
-          <SkillElement
-            imageSrc={NN}
-            area={isSmallScreen ? "4 / 3 / 4 / span 16" : "2 / 21 / 2 / span 7"}
-            skill="Model Building"
-          />
-          <SkillElement
-            imageSrc={git}
-            area={
-              isSmallScreen ? "4 / 21 / 4 / span 16" : "2 / 29 / 2 / span 7"
-            }
-            skill="Git"
-          />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="skillsWrap">
+			<div className="skillsGrid">
+				<div style={{ display: "contents" }}>
+					<SkillElement
+						imageSrc={Py}
+						area={isSmallScreen ? "1 / 3 / 1 / span 16" : "1 / 5 / 1 / span 7"}
+						skill="Python"
+					/>
+					<SkillElement
+						imageSrc={R}
+						area={
+							isSmallScreen ? "1 / 21 / 1 / span 16" : "1 / 13 / 1 / span 7"
+						}
+						skill="R"
+					/>
+					<SkillElement
+						imageSrc={TF}
+						area={isSmallScreen ? "2 / 3 / 2 / span 16" : "1 / 21 / 1 / span 7"}
+						skill="Tensorflow"
+					/>
+					<SkillElement
+						imageSrc={PT}
+						area={
+							isSmallScreen ? "2 / 21 / 2 / span 16" : "1 / 29 / 1 / span 7"
+						}
+						skill="PyTorch"
+					/>
+					<SkillElement
+						imageSrc={S}
+						area={isSmallScreen ? "3 / 3 / 3 / span 16" : "2 / 5 / 2 / span 7"}
+						skill="Statistics"
+					/>
+					<SkillElement
+						imageSrc={brain}
+						area={
+							isSmallScreen ? "3 / 21 / 3 / span 16" : "2 / 13 / 2 / span 7"
+						}
+						skill="Deep Learning"
+					/>
+					<SkillElement
+						imageSrc={NN}
+						area={isSmallScreen ? "4 / 3 / 4 / span 16" : "2 / 21 / 2 / span 7"}
+						skill="Model Building"
+					/>
+					<SkillElement
+						imageSrc={git}
+						area={
+							isSmallScreen ? "4 / 21 / 4 / span 16" : "2 / 29 / 2 / span 7"
+						}
+						skill="Git"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Skills;
